21a4dc23d6363a5e10b55a0e7784120f36ed2670
brungran <brungran@gmail.com>
2022-11-18 12:21:41 -0300

    list commits

---
+++ src/components/Commit.vue
@@ -0,0 +1,11 @@
+<template>
+    <td>{{commit.commit.message}}</td>
+    <td>{{commit.commit.author.date}}</td>
+    <td>{{commit.commit.author.name}}</td>
+</template>
+
+<script setup>
+const props = defineProps({
+    commit: Object
+})
+</script>
