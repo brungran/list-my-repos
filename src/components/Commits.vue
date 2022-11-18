7790a924bceff0fa0647d63f6e3559304fb1a2c1
brungran <brungran@gmail.com>
2022-11-18 20:07:03 -0300

    Commits sort

--- src/components/Commits.vue
+++ src/components/Commits.vue
@@ -1,24 +1,64 @@
 <template>
     <RouterLink to="/">Go back</RouterLink>
+    <div>
+        <button @click="commits = alphabetical">Alphabetical Order</button>
+    </div>
+    <div>
+        <button @click="commits = orderByDate">Order by Date</button>
+    </div>
     <h3>Commits fetched:</h3>
     <table>
         <th>Message</th>
         <th>Date and Time</th>
         <th>Author</th>
         
-        <tr :key="commit.id" v-for="commit in commitStore.commits">
+        <tr :key="commit.id" v-for="commit in commits">
             <Commit :commit="commit" />
         </tr>
     </table>
 </template>
 
-<script setup>
+<script>
     import Commit from '@/components/Commit.vue'
     import { useCommitStore } from '@/stores/CommitStore'
     import { useRoute } from 'vue-router';
-
-    const route = useRoute();
-
+    
     const commitStore = useCommitStore()
-    commitStore.list(route.params.name)
+    
+    export default{
+        data(){
+            return{
+                commits: []
+            }
+        },
+        async created(){
+            await commitStore.list(useRoute().params.name)
+            this.commits = commitStore.all //getter that maps through the state to convert dates
+        },
+        components:{
+            Commit
+        },
+        computed:{
+            alphabetical(){
+                console.log('alphabetical')
+                let alphabetical = this.commits
+                alphabetical = alphabetical.sort((a,b) => {
+                    if(a.commit.message.toLowerCase() < b.commit.message.toLowerCase()) return -1
+                    if(a.commit.message.toLowerCase() > b.commit.message.toLowerCase()) return 1
+                    return 0
+                })
+                return alphabetical
+            },
+            orderByDate(){
+                console.log('orderByDate')
+                let orderByDate = this.commits
+                orderByDate = orderByDate.sort((a,b) => {
+                    if(a.commit.author.date > b.commit.author.date) return -1
+                    if(a.commit.author.date < b.commit.author.date) return 1
+                    return 0
+                })
+                return orderByDate
+            },
+        }
+    }
 </script>
--- src/stores/CommitStore.js
+++ src/stores/CommitStore.js
@@ -5,19 +5,52 @@ export const useCommitStore = defineStore('commitStore', {
   state: () => ({
     commits: []
   }),
+  getters:{
+    all(){
+      return this.commits.map((commit) => {
+        let date = new Date(commit.commit.author.date)
+        // let formattedDate = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+        let formattedDate = date.toLocaleDateString("pt-BR", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        })
+        return {
+          ...commit,
+          "commit":{
+            ...commit.commit,
+            "author":{
+              ...commit.commit.author,
+              "date": formattedDate
+            },
+            "committer":{
+              ...commit.commit.committer,
+              "date": formattedDate
+            }
+          }
+        }
+      })
+    }
+  },
   actions:{
     async list(repo){
       const octokit = new Octokit({
         auth: import.meta.env.VITE_SOME_KEY
       })
 
-      const res = await octokit.request('GET /repos/'+import.meta.env.VITE_GITHUB_USER+'/'+repo+'/commits', {
-        owner: import.meta.env.VITE_GITHUB_USER,
-        repo: repo
-      })
-
-
-      this.commits = res.data
+      try {
+        const res = await octokit.request('GET /repos/'+import.meta.env.VITE_GITHUB_USER+'/'+repo+'/commits', {
+          owner: import.meta.env.VITE_GITHUB_USER,
+          repo: repo
+        })
+        if(res.status === 200) this.commits = res.data
+      } catch (error) {
+        this.commits = []
+        return error
+      }
     }
   }
 })
